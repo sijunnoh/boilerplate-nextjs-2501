019939e7a8325a2a2c7ed122bb19a4d7b07c95d7
Sijun Noh <sijun.noh@gmail.com>
2025-02-05 23:42:18 +0900

    feat: remove unnecessary fonts
    - remove default fonts in layout
    - change lang to kr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "Boilerplate",
@@ -24,12 +13,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="kr">
+      <body className={`antialiased`}>{children}</body>
     </html>
   )
 }
